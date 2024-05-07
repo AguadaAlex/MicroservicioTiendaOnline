cbea06c32170882c4739534d22e9dfa7d35f1d91
Alex <alex.david.aguada.jura@gmail.com>
2024-05-07 10:28:11 -0300

    Lombok

--- core/src/main/java/edu/tienda/core/controllers/ProductoControllerRest.java
+++ core/src/main/java/edu/tienda/core/controllers/ProductoControllerRest.java
@@ -1,23 +1,41 @@
 package edu.tienda.core.controllers;
 
+import edu.tienda.core.configurations.ConfigurationParameters;
 import edu.tienda.core.domain.Producto;
 import edu.tienda.core.services.ProductoService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/productos")
 public class ProductoControllerRest {
     @Autowired
+    @Lazy
     private ProductoService productosService;
+
+    @Autowired
+    private ConfigurationParameters configurationParameters;
     @GetMapping
     public ResponseEntity<?> getProductos(){
+        System.out.println("Parameters: "+ configurationParameters.toString());
         List<Producto> productos=productosService.getProductos();
-        System.out.println(productos);
         return ResponseEntity.ok(productos);
     }
+    @GetMapping("/fake-productos")
+    public ResponseEntity<?> fakeProductosAPI(){
+        List<Producto> productos = new ArrayList<>(Arrays.asList(
+           new Producto(1,"Camiseta Juventus", 1200.0, 4),
+           new Producto(2,"Camiseta River Plate", 1000.0, 8),
+           new Producto(3,"Camiseta Boca Junior", 900.0, 1)
+        ));
+        return ResponseEntity.ok(productos);
+    };
 }
--- core/src/main/java/edu/tienda/core/domain/Producto.java
+++ core/src/main/java/edu/tienda/core/domain/Producto.java
@@ -1,5 +1,12 @@
 package edu.tienda.core.domain;
 
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Producto {
     private Integer id;
     private String nombre;
@@ -13,35 +20,4 @@ public class Producto {
         this.stock = stock;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
-    }
 }
